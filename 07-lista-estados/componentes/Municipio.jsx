19e26bdd5a4a7366f879ad9606746ceff0f89d76
Kaio Victor da Silva <kvictor1711@gmail.com>
2025-04-04 20:23:52 -0300

    lista com componentes

---
+++ 07-lista-estados/componentes/Municipio.jsx
@@ -0,0 +1,18 @@
+import { StyleSheet } from 'react-native'
+import React from 'react'
+import { Avatar, Card } from 'react-native-paper';
+
+export default function Municipio(props) {
+
+const {nome, imagem} = props
+
+    return (
+        <Card.Title
+            title="Card Title"
+            subtitle="Card Subtitle"
+            left={(props) => <Avatar.Image {...props} source={{ uri: imagem }} />}
+        />
+    )
+}
+
+const styles = StyleSheet.create({})
